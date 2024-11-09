f9ccfe44df37d3f51bf62158b18fedbb67f56815
MinhQuan21112002 <93760661+MinhQuan21112002@users.noreply.github.com>
2024-11-09 17:25:10 +0700

    Update SwaggerConfig.java

--- src/main/java/com/java08/quanlituyendung/config/SwaggerConfig.java
+++ src/main/java/com/java08/quanlituyendung/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.java08.quanlituyendung.config;
 
-import com.google.common.collect.Ordering;
 import com.java08.quanlituyendung.auth.AuthenticationService;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -23,33 +22,38 @@ public class SwaggerConfig {
     @Autowired
     AuthenticationService authenticationService;
 
+    // Tạo SecurityScheme cho Bearer Authentication với JWT
     private SecurityScheme createAPIKeyScheme() {
-        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
                 .bearerFormat("JWT")
                 .scheme("Bearer");
     }
 
-
+    // Cấu hình Swagger Docket (SpringFox Swagger 2)
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.java08.quanlituyendung"))
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.java08.quanlituyendung"))  // Chỉ định package của API
+                .paths(PathSelectors.any())  // Áp dụng cho tất cả các endpoint
                 .build();
     }
 
+    // Cấu hình OpenAPI (Swagger 3) với JWT Bearer Authentication
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
                 .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
-                .info(new Info().title(" recruitment Api")
-                        .description("Quản Lý Tuyển Dụng ")
-                        .version("1.0").contact(new Contact().name("Nguyen Le Quoc Khanh")
-                                .email("nguyenkhanh2kpi@gmail.com")).license(new License()
-                                .name("license Api").url("https://www.facebook.com/profile.php?id=100058506963756")));
+                .info(new Info()
+                        .title("Recruitment API")
+                        .description("Quản lý tuyển dụng")
+                        .version("1.0")
+                        .contact(new Contact().name("Nguyen Le Quoc Khanh")
+                                .email("nguyenkhanh2kpi@gmail.com"))
+                        .license(new License()
+                                .name("API License")
+                                .url("https://www.facebook.com/profile.php?id=100058506963756")));
     }
-
-
 }
