7079d54968172c4812d422b2f8e88a6e9f92da96
MinhQuan21112002 <93760661+MinhQuan21112002@users.noreply.github.com>
2024-12-05 11:15:06 +0700

    Update UserAccountCustomForReccerResponseDTO.java

--- src/main/java/com/java08/quanlituyendung/dto/UserAccountPayload/UserAccountCustomForReccerResponseDTO.java
+++ src/main/java/com/java08/quanlituyendung/dto/UserAccountPayload/UserAccountCustomForReccerResponseDTO.java
@@ -1,6 +1,7 @@
 package com.java08.quanlituyendung.dto.UserAccountPayload;
 
 
+import com.java08.quanlituyendung.entity.Status;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,4 +18,5 @@ public class UserAccountCustomForReccerResponseDTO {
     private String fullName;
     private String username;
     private Long reccerID;
+    private Status status;
 }
