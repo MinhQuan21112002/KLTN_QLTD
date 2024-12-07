e954a78d4a2a011bd86bc581f55477e0763332f0
MinhQuan21112002 <93760661+MinhQuan21112002@users.noreply.github.com>
2024-12-07 22:34:54 +0700

    Update JobPostingController.java

--- src/main/java/com/java08/quanlituyendung/controller/JobPostingController.java
+++ src/main/java/com/java08/quanlituyendung/controller/JobPostingController.java
@@ -12,7 +12,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "${allowed.origin}")
+@CrossOrigin(origins = "https://kltnfrontendqltd-production.up.railway.app")
 @RequestMapping(value = "/job-posting")
 @Tag(name = "Job Posting")
 public class JobPostingController {
