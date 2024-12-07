d3e0b6c0c0ed2031fcd5b4299fcf4e4e0ba2d9c0
MinhQuan21112002 <93760661+MinhQuan21112002@users.noreply.github.com>
2024-12-07 22:35:44 +0700

    Update EventController.java

--- src/main/java/com/java08/quanlituyendung/controller/EventController.java
+++ src/main/java/com/java08/quanlituyendung/controller/EventController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@CrossOrigin(origins = "https://kltnfrontendqltd-production.up.railway.app")
 @RequestMapping("/event")
 @RequiredArgsConstructor
 @Tag(name = "Event")
