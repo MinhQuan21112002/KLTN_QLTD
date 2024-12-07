0cfef503d94923f4792ae558ae7a62587c145cec
MinhQuan21112002 <93760661+MinhQuan21112002@users.noreply.github.com>
2024-12-07 22:41:52 +0700

    Update UserAccountController.java

--- src/main/java/com/java08/quanlituyendung/controller/UserAccountController.java
+++ src/main/java/com/java08/quanlituyendung/controller/UserAccountController.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "https://kltnfrontendqltd-production.up.railway.app")
 @RequestMapping("/user")
 @RequiredArgsConstructor
 @Tag(name = "User Account")
